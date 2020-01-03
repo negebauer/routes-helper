6a109a87ff80b388a8c8ce59fa6aebf86aa0042c
Nicolás Gebauer <negebauerm@gmail.com>
2020-01-03 03:35:37 -0300

    example

--- example/index.js
+++ example/index.js
@@ -1,19 +1,42 @@
+/* eslint-disable no-console */
 const fs = require('fs')
 const path = require('path')
+const buildRoutes = require('../src/build-routes')
+const routesToJson = require('../src/routes-to-json')
 
-const output = path.resolve(__dirname, 'output.json')
+const output = path.resolve(__dirname, 'routes.json')
 
-const routes = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: {
-    a: 1,
-    b: 2,
+const routes = buildRoutes({
+  home: { __pathName: '' },
+  feed: 'cool-feed',
+  session: {
+    login: '',
   },
-}
+  dashboard: {
+    graphs: '',
+    charts: '',
+  },
+  users: {
+    user: {
+      __pathName: ':userId',
+      friends: '',
+    },
+  },
+  bestPlaces: {
+    __pathName: 'best_places',
+    place: ':placeId',
+  },
+})
 
-// eslint-disable-next-line no-console
-console.dir(routes)
+const json = routesToJson(routes)
 
-fs.writeFileSync(output, `${JSON.stringify(routes, null, 2)}\n`)
+console.log('routes object')
+console.log(routes)
+
+console.log('json routes object')
+console.log(json)
+
+console.log(`routes.home()\n\t${routes.home()}`)
+console.log(`routes.users.user.friends()\n\t${routes.users.user.friends()}`)
+
+fs.writeFileSync(output, `${JSON.stringify(json, null, 2)}\n`)
