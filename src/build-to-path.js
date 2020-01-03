97cc343a43ded1c909bc1e2692340ac2f9793343
Nicolás Gebauer <negebauerm@gmail.com>
2020-01-03 04:14:00 -0300

    comments

--- src/build-to-path.js
+++ src/build-to-path.js
@@ -2,11 +2,41 @@ const pathToRegexp = require('path-to-regexp')
 
 const toPathCache = {}
 
+/**
+ * Calls the cached path-to-regexp function for the provided route
+ * for generating paths. If there's no compiled function for the route
+ * then it compiles it before calling it.
+ *
+ * @param {string} route A route
+ * @param {Object} params Params to pass to path-to-regexp to build the path
+ */
 function toPathCached(route, params) {
   if (!toPathCache[route]) toPathCache[route] = pathToRegexp.compile(route)
   return toPathCache[route](params)
 }
 
+/**
+ * Builds a routes similar object that binds to calls to generate paths
+ * using `path-to-regexp`. This allows the generation of routes
+ * that include parameters.
+ *
+ * Example
+ *
+ * ```js
+ * const routes = buildRoutes({
+ *  users: {
+ *    user: { __pathName: ':userId' }
+ *  }
+ * })
+ *
+ * const toPath = buildToPath(routes)
+ *
+ * toPath.users.user({ userId: 1 })
+ * // /users/1
+ * ```
+ *
+ * @param {Object} routes The result of calling `buildRoutes` on a routes object
+ */
 function buildToPath(routes) {
   const routesToPathObject = {}
 
